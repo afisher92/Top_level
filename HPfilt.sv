67f685df76a6e0cc8f8339198e067e37232382af
afisher92 <fisheand92@gmail.com>
2015-12-10 21:42:06 -0600

    Update HPfilt.sv

--- HPfilt.sv
+++ HPfilt.sv
@@ -6,28 +6,18 @@ input [15:0] rght_in, lft_in;
 output wire [15:0]rght_out, lft_out;
 
 reg[15:0]dout;
-reg[31:0]laccum, raccum, lmult, rmult;
+reg[31:0]laccum, raccum;
 reg[9:0]addr;
 reg FF_seq, pos_seq;
 
 //Instantiates the ROM
 ROM_HP HP(.dout(dout), .clk(clk), .addr(addr));
 
-/*Implements coefficient multiplication
-always_ff @(posedge clk, negedge rst_n)
- if(!rst_n) begin
-  rmult <= 32'h00000000;
-  lmult <= 32'h00000000;
- end else begin
-  rmult <= dout * rght_in;
-  lmult <= dout * lft_in;
- end */
-
 //Implements addr
 always_ff @(posedge clk, negedge rst_n)
  if(!rst_n)
   addr <= 10'h000;
- else if(!sequencing)
+ else if(pos_seq)
 	    addr <= 10'h000;
 	   else
 	    addr <= addr + 1;
